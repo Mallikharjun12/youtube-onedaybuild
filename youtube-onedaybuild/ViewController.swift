9a5f625f274ac48c6d7b8f3a2e035c68954ad389
Kondal <yedukondalu.n@anynews.co.in>
2022-08-21 11:03:27 +0530

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by admin on 21/08/22.
+//  Copyright © 2022 konda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
