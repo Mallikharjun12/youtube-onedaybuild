2dadb603935a575c1940fa697626f7bbc730bc4b
Mallikharjun12 <yedukondalu.n@anynews.co.in>
2022-08-21 21:36:09 +0530

    Added ConstantsTemplate.swift file

---
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by admin on 21/08/22.
+//  Copyright © 2022 konda. All rights reserved.
+//
+
+// Uncomment the code and add your API_KEY and PLAYLIST_ID
+
+import Foundation
+
+//struct Constants {
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//}
